35f62cd7ea9c06069b174c69e0c433f8ae939d6f
georgeherbert <george.herbert@icloud.com>
2020-10-05 19:34:09 +0100

    Completed question 5

--- intro-to-go-lab-1/hello.go
+++ intro-to-go-lab-1/hello.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func hello(i int) {
+	fmt.Println("Hello from goroutine", i)
+}
 
 func main() {
-	for i := 0; i < 20; i++ {
-		fmt.Println("Hello World")
+	for i := 0; i < 5; i++ {
+		go hello(i)
+		time.Sleep(1 * time.Second)
 	}
 }
