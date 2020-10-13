5abfe746ae646e76f778a9ed819e22891dd12a34
georgeherbert <george.herbert@icloud.com>
2020-10-13 14:37:25 +0100

    Completed p1s3

--- distributed-lab-1/part1/client.go
+++ distributed-lab-1/part1/client.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+	"flag"
 	)
 
 func main() {
+	msgPtr := flag.String("msg", "No message supplied", "Message to send")
+	flag.Parse()
 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
-	fmt.Fprintf(conn, "Hi! This is the client.")
+	fmt.Fprintf(conn, *msgPtr)
 }
