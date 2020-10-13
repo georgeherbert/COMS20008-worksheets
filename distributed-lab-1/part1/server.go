6cc41dcf0545dfcbcf5959f5178c7e0230b00838
georgeherbert <george.herbert@icloud.com>
2020-10-13 14:34:13 +0100

    Completed p1s2

--- distributed-lab-1/part1/server.go
+++ distributed-lab-1/part1/server.go
@@ -6,10 +6,16 @@ import (
 	"bufio"
 	)
 
-func main() {
-	ln, _ := net.Listen("tcp", ":8080")
-	conn, _ := ln.Accept()
-	reader := bufio.NewReader(conn)
+func handleConnection(conn *net.Conn) {
+	reader := bufio.NewReader(*conn)
 	msg, _ := reader.ReadString('\n')
 	fmt.Println(msg)
+}
+
+func main() {
+	ln, _ := net.Listen("tcp", ":8080")
+	for {
+		conn, _ := ln.Accept()
+		go handleConnection(&conn)
+	}
 }
