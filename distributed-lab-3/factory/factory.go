3598416fb5524070b0703c0fe77802d4624fa42e
georgeherbert <george.herbert@icloud.com>
2020-11-07 15:23:42 +0000

    Completed a2a

--- distributed-lab-3/factory/factory.go
+++ distributed-lab-3/factory/factory.go
@@ -19,16 +19,28 @@ func (f *Factory) Multiply(req stubs.Pair, res *stubs.JobReport) (err error) {
 	return
 }
 
+func (f *Factory) Divide(req stubs.Pair, res *stubs.JobReport) (err error) {
+	res.Result = req.X / req.Y
+	fmt.Println(strconv.Itoa(req.X) + " / " + strconv.Itoa(req.Y) + " = " + strconv.Itoa(res.Result))
+	return
+}
+
 func main(){
 	pAddr := flag.String("ip", "127.0.0.1:8050", "IP and port to listen on")
 	brokerAddr := flag.String("broker","127.0.0.1:8030", "Address of broker instance")
 	flag.Parse()
 	//TODO: You'll need to set up the RPC server, and subscribe to the running broker instance.
+
 	client, _ := rpc.Dial("tcp", *brokerAddr)
 	defer client.Close()
-	subscribe := stubs.Subscription{Topic: "multiply", FactoryAddress: *pAddr, Callback: "Factory.Multiply"}
-	status := new(stubs.StatusReport)
-	client.Go(stubs.Subscribe, subscribe, status, nil)
+
+	subscribeMul := stubs.Subscription{Topic: "multiply", FactoryAddress: *pAddr, Callback: "Factory.Multiply"}
+	statusMul := new(stubs.StatusReport)
+	client.Go(stubs.Subscribe, subscribeMul, statusMul, nil)
+
+	subscribeDiv := stubs.Subscription{Topic: "divide", FactoryAddress: *pAddr, Callback: "Factory.Divide"}
+	statusDiv := new(stubs.StatusReport)
+	client.Go(stubs.Subscribe, subscribeDiv, statusDiv, nil)
 
 	rpc.Register(&Factory{})
 	listener, _ := net.Listen("tcp", *pAddr)
