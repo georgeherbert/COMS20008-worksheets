772bb3a551bbc370caae80553364004b798aa22a
georgeherbert <george.herbert@icloud.com>
2020-11-07 15:44:47 +0000

    Completed q2b

--- distributed-lab-3/factory/factory.go
+++ distributed-lab-3/factory/factory.go
@@ -9,18 +9,33 @@ import (
 	"strconv"
 )
 
+var pipe = make(chan int)
+
+func pipeline(client *rpc.Client) {
+	for {
+		x := <-pipe
+		y := <-pipe
+		towork := stubs.PublishRequest{Topic: "divide", Pair: stubs.Pair{x, y}}
+		status := new(stubs.StatusReport)
+		client.Call(stubs.Publish, towork, status)
+	}
+}
+
 type Factory struct {}
 
 //TODO: Define a Multiply function to be accessed via RPC.
 //Check the previous weeks' examples to figure out how to do this.
 func (f *Factory) Multiply(req stubs.Pair, res *stubs.JobReport) (err error) {
-	res.Result = req.X * req.Y
+	result := req.X * req.Y
+	res.Result = result
+	pipe <- result
 	fmt.Println(strconv.Itoa(req.X) + " * " + strconv.Itoa(req.Y) + " = " + strconv.Itoa(res.Result))
 	return
 }
 
 func (f *Factory) Divide(req stubs.Pair, res *stubs.JobReport) (err error) {
-	res.Result = req.X / req.Y
+	result := req.X / req.Y
+	res.Result = result
 	fmt.Println(strconv.Itoa(req.X) + " / " + strconv.Itoa(req.Y) + " = " + strconv.Itoa(res.Result))
 	return
 }
@@ -32,7 +47,11 @@ func main(){
 	//TODO: You'll need to set up the RPC server, and subscribe to the running broker instance.
 
 	client, _ := rpc.Dial("tcp", *brokerAddr)
+	status := new(stubs.StatusReport)
 	defer client.Close()
+	client.Call(stubs.CreateChannel, stubs.ChannelRequest{Topic: "divide", Buffer: 10}, status)
+
+	go pipeline(client)
 
 	subscribeMul := stubs.Subscription{Topic: "multiply", FactoryAddress: *pAddr, Callback: "Factory.Multiply"}
 	statusMul := new(stubs.StatusReport)
